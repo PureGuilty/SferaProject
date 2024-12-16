138fe3f0c6683702ad5e65466d814d217a96bf56
PureGuilty <flekser334@gmail.com>
2024-12-16 23:44:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function calculatePayment() {
         document.getElementById('result').innerText = 'Пожалуйста, введите корректные данные.';
     }
 }
+function fetchKeyRate() {
+    fetch('https://api.cbr.ru/v1/keyrate')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const keyRate = data[0].value; // предположим, что ставка находится в первом элементе
+            document.getElementById('keyRate').innerText = `Ключевая ставка: ${keyRate}%`;
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
+
+// Запускаем функцию для получения ключевой ставки
+fetchKeyRate();
+
+// Обновление каждые 1 час (3600000 миллисекунд)
+setInterval(fetchKeyRate, 3600000);
